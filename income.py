a4c4cbb56cd3741e151672716d41a4eb13ab82df
Danny Andersen <danny_andersen@yahoo.com>
2019-09-04 21:53:08 +0100

    Dividend calc

---
+++ income.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+import json
+import datetime
+
+def get_jsonparsed_data(url):
+    """
+    Receive the content of ``url``, parse it as JSON and return the object.
+
+    Parameters
+    ----------
+    url : str
+
+    Returns
+    -------
+    dict
+    """
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+url = ("https://financialmodelingprep.com/api/financials/income-statement/AAPL?period=quarter&datatype=json")
+income = get_jsonparsed_data(url)
+print(f"Stock: {income['symbol']}")
+diviStats = []
+for point in income['financials']:
+    dividend = float(point['Dividend per Share'])
+    day = datetime.datetime.strptime(point['date'], "%Y-%m-%d")
+    if dividend != 0:
+        diviCover = float(point['EPS']) / dividend
+    else:   
+        diviCover = "N/A"
+    #print(f"Date: {day} Dividend: {dividend:.3f} Dividend Cover {diviCover:.3f}")
+    diviPoint = {'date': day, 'dividend': dividend, 'divi cover': diviCover} 
+    diviStats.append(diviPoint)
+ 
+print(diviStats)
