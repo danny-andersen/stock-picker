97e60aa997ed5765fe7b4694cf8722ef12906437
Danny Andersen <danny.andersen@yahoo.com>
2021-01-14 08:56:30 +0000

    Increased executors

--- sparkSubmit.sh
+++ sparkSubmit.sh
@@ -4,6 +4,6 @@ python -m compileall . && zip -r processStock.zip *.py *.ini *.txt
 /data/spark/bin/spark-submit \
 	--master yarn  \
 	--executor-memory 512M  \
-	--num-executors 8  \
+	--num-executors 12  \
 	--properties-file spark-props.conf \
 	--py-files processStock.zip $1
